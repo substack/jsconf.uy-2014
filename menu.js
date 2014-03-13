b1398d04eb929836fa221c299f405933eb1eb1a6
James Halliday <mail@substack.net>
2014-03-13 11:25:57 -0700

    menu history

--- menu.js
+++ menu.js
@@ -1,23 +1,51 @@
 #!/usr/bin/env node
 
-var menu = require('terminal-menu')({ width: 60, x: 4, y: 2 });
+var fs = require('fs');
+var Menu = require('terminal-menu');
+
+var menu = Menu({ width: 60, x: 4, y: 2 });
 menu.reset();
 
 menu.write('TINY FRONTEND MODULES [ jsconf.uy 2014 ]\n');
 menu.write('----------------------------------------\n');
 
-var completed = [];
-try { completed = require('./completed.json') }
+var saved = { completed: [] };
+try { saved = require('./saved.json') }
 catch (err) {}
 
 require('./order.json').forEach(function (item) {
-    menu.add(item.toUpperCase());
+    var msg = item.toUpperCase();
+    if (saved.completed.indexOf(item) >= 0) {
+        msg = asCompleted(item);
+    }
+    menu.add(msg);
 });
 menu.write('----------------------------------------\n');
+menu.add('RESET');
 menu.add('EXIT');
 
 menu.on('select', function (label) {
+    var item = label.split(/\s{2,}/)[0].toLowerCase();
+    if (item === 'exit') { }
+    else if (item === 'reset') {
+        fs.unlink(__dirname + '/saved.json');
+    }
+    else {
+        saved.completed.push(item);
+        saved.last = asCompleted(item);
+        fs.writeFile(__dirname + '/saved.json', JSON.stringify(saved));
+        
+        menu.close();
+        console.log('SELECTED: ' + label);
+    }
     menu.close();
-    console.log('SELECTED: ' + label);
 });
 menu.createStream().pipe(process.stdout);
+menu.jump(saved.last);
+
+function asCompleted (item) {
+    var msg = item.toUpperCase();
+    return msg + Array(40 - msg.length + 1 - 11).join(' ')
+        + '[COMPLETED]'
+    ;
+}
